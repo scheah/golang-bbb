7ee8e5395c245143c1dda48362229aaca36c1c04
Sebastian Cheah <sebcheah@gmail.com>
2015-06-11 03:15:43 -0700

    Start Optimizing
    First setup library, read d2xx/ReadMe.txt
    
    Running new program notes:
    Kill orig. VCP drivers before using D2XX
    rmmod ftdi_sio
    rmmod usbserial

---
+++ optimized/endpoint/goendpoints.go
@@ -0,0 +1,466 @@
+package main
+
+//#cgo LDFLAGS: -L. -lftd2xx -Wl,-rpath /usr/local/lib
+//#include <stdint.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <sys/time.h>
+//#include "ftd2xx.h"
+//
+//FT_STATUS	ftStatus;
+//FT_HANDLE	ftHandle0;
+//
+//char * readMsg() {
+//	DWORD RxBytes = 27;
+//	DWORD BytesReceived;
+//	char * msg = malloc(RxBytes);
+//	ftStatus = FT_Read(ftHandle0,msg,RxBytes,&BytesReceived);	
+//	if (ftStatus == FT_OK) {
+//		if (BytesReceived == RxBytes) {
+//			printf("reading: %s\n", msg);
+//			printf("FT_Read OK\n", msg);
+//			return msg;
+//		}
+//		else {
+//			printf("FT_Read Timeout\n");
+//		}
+//	}
+//	else {
+//		printf("FT_Read Failed\n");
+//	}
+// 	
+//}
+//
+//void writeMsg(char * msg) {
+//	DWORD BytesWritten;
+//	//char TxBuffer[64]; // Contains data to write to device
+//	//strncpy(TxBuffer, msg, sizeof(TxBuffer));
+// 	//TxBuffer[sizeof(TxBuffer) - 1] = '\0';
+//	printf("writing: %s\n", msg);
+//	ftStatus = FT_Write(ftHandle0, msg, 27, &BytesWritten);
+//	if (ftStatus == FT_OK) {
+//		printf("FT_Write OK\n");
+//	}
+//	else {
+//		printf("FT_Write Failed\n");
+//	}
+//}
+//
+//
+//int setup()
+//{
+//	int iport;
+//	static FT_PROGRAM_DATA Data;
+//	static FT_DEVICE ftDevice;
+//	DWORD libraryVersion = 0;
+//	int retCode = 0;
+//
+//	ftStatus = FT_GetLibraryVersion(&libraryVersion);
+//	if (ftStatus == FT_OK)
+//	{
+//		printf("Library version = 0x%x\n", (unsigned int)libraryVersion);
+//	}
+//	else
+//	{
+//		printf("Error reading library version.\n");
+//		return 1;
+//	}
+//
+//	iport = 0;
+//
+//	printf("Opening port %d\n", iport);
+//	
+//	ftStatus = FT_Open(iport, &ftHandle0);
+//	if(ftStatus != FT_OK) {
+//		/* 
+//			This can fail if the ftdi_sio driver is loaded
+//		 	use lsmod to check this and rmmod ftdi_sio to remove
+//			also rmmod usbserial
+//		 */
+//		printf("FT_Open(%d) failed\n", iport);
+//		return 1;
+//	}
+//
+//	printf("FT_Open succeeded.  Handle is %p\n", ftHandle0);
+//
+//  ftStatus = FT_ResetDevice(ftHandle0);
+//  if (ftStatus == FT_OK)
+//  	printf("FT_ResetDevice OK\n");
+//  else
+//      printf("FT_ResetDevice FAILED\n");
+//	ftStatus = FT_GetDeviceInfo(ftHandle0,
+//	                            &ftDevice,
+//	                            NULL,
+//	                            NULL,
+//	                            NULL,
+//	                            NULL); 
+//	if (ftStatus != FT_OK) 
+//	{ 
+//		printf("FT_GetDeviceType FAILED!\n");
+//		retCode = 1;
+//		goto exit;
+//	}  
+//
+//	printf("FT_GetDeviceInfo succeeded.  Device is type %d.\n", 
+//	       (int)ftDevice);
+//
+//	/* MUST set Signature1 and 2 before calling FT_EE_Read */
+//	Data.Signature1 = 0x00000000;
+//	Data.Signature2 = 0xffffffff;
+//	Data.Manufacturer = (char *)malloc(256); /* E.g "FTDI" */
+//	Data.ManufacturerId = (char *)malloc(256); /* E.g. "FT" */
+//	Data.Description = (char *)malloc(256); /* E.g. "USB HS Serial Converter" */
+//	Data.SerialNumber = (char *)malloc(256); /* E.g. "FT000001" if fixed, or NULL */
+//	if (Data.Manufacturer == NULL ||
+//	    Data.ManufacturerId == NULL ||
+//	    Data.Description == NULL ||
+//	    Data.SerialNumber == NULL)
+//	{
+//		printf("Failed to allocate memory.\n");
+//		retCode = 1;
+//		goto exit;
+//	}
+//
+//	ftStatus = FT_EE_Read(ftHandle0, &Data);
+//	if(ftStatus != FT_OK) {
+//		printf("FT_EE_Read failed\n");
+//		retCode = 1;
+//		goto exit;
+//	}
+//
+//	printf("FT_EE_Read succeeded.\n\n");
+//	
+//	printf("Signature1 = %d\n", (int)Data.Signature1);			
+//	printf("Signature2 = %d\n", (int)Data.Signature2);			
+//	printf("Version = %d\n", (int)Data.Version);				
+//								
+//	printf("VendorId = 0x%04X\n", Data.VendorId);				
+//	printf("ProductId = 0x%04X\n", Data.ProductId);
+//	printf("Manufacturer = %s\n", Data.Manufacturer);			
+//	printf("ManufacturerId = %s\n", Data.ManufacturerId);		
+//	printf("Description = %s\n", Data.Description);			
+//	printf("SerialNumber = %s\n", Data.SerialNumber);			
+//	printf("MaxPower = %d\n", Data.MaxPower);				
+//	printf("PnP = %d\n", Data.PnP) ;					
+//	printf("SelfPowered = %d\n", Data.SelfPowered);			
+//	printf("RemoteWakeup = %d\n", Data.RemoteWakeup);			
+//	if (ftDevice == FT_DEVICE_232R)
+//	{
+//		/* Rev 6 (FT232R) extensions */
+//		printf("232R:\n");
+//		printf("-----\n");
+//		printf("\tUseExtOsc = 0x%X\n", Data.UseExtOsc);			// Use External Oscillator
+//		printf("\tHighDriveIOs = 0x%X\n", Data.HighDriveIOs);			// High Drive I/Os
+//		printf("\tEndpointSize = 0x%X\n", Data.EndpointSize);			// Endpoint size
+//
+//		printf("\tPullDownEnableR = 0x%X\n", Data.PullDownEnableR);		// non-zero if pull down enabled
+//		printf("\tSerNumEnableR = 0x%X\n", Data.SerNumEnableR);		// non-zero if serial number to be used
+//
+//		printf("\tInvertTXD = 0x%X\n", Data.InvertTXD);			// non-zero if invert TXD
+//		printf("\tInvertRXD = 0x%X\n", Data.InvertRXD);			// non-zero if invert RXD
+//		printf("\tInvertRTS = 0x%X\n", Data.InvertRTS);			// non-zero if invert RTS
+//		printf("\tInvertCTS = 0x%X\n", Data.InvertCTS);			// non-zero if invert CTS
+//		printf("\tInvertDTR = 0x%X\n", Data.InvertDTR);			// non-zero if invert DTR
+//		printf("\tInvertDSR = 0x%X\n", Data.InvertDSR);			// non-zero if invert DSR
+//		printf("\tInvertDCD = 0x%X\n", Data.InvertDCD);			// non-zero if invert DCD
+//		printf("\tInvertRI = 0x%X\n", Data.InvertRI);				// non-zero if invert RI
+//
+//		printf("\tCbus0 = 0x%X\n", Data.Cbus0);				// Cbus Mux control
+//		printf("\tCbus1 = 0x%X\n", Data.Cbus1);				// Cbus Mux control
+//		printf("\tCbus2 = 0x%X\n", Data.Cbus2);				// Cbus Mux control
+//		printf("\tCbus3 = 0x%X\n", Data.Cbus3);				// Cbus Mux control
+//		printf("\tCbus4 = 0x%X\n", Data.Cbus4);				// Cbus Mux control
+//
+//		printf("\tRIsD2XX = 0x%X\n", Data.RIsD2XX); // non-zero if using D2XX
+//	}
+//	ftStatus = FT_SetBaudRate(ftHandle0, 115200); // Set baud rate to 115200
+//	if (ftStatus == FT_OK) {
+//		printf("FT_SetBaudRate OK\n");
+//	}
+//	else {
+//		printf("FT_SetBaudRate Failed\n");
+//	}
+//	ftStatus = FT_SetFlowControl(ftHandle0, FT_FLOW_RTS_CTS, 0x11, 0x13);
+//	if (ftStatus == FT_OK) {
+//		printf("FT_SetFlowControl OK\n");
+//	}
+//	else {
+//		printf("FT_SetFlowControl Failed\n");
+//	}
+//	UCHAR LatencyTimer = 1;
+//	ftStatus = FT_SetLatencyTimer(ftHandle0, LatencyTimer );
+//	if (ftStatus == FT_OK) {
+//		printf("Set LatencyTimer: %u\n", LatencyTimer );
+//	}
+//	else {
+//		printf("FT_SetLatencyTimer failed\n");
+//		retCode = 1;
+//		goto exit;
+//	}
+//	DWORD TransferSize = 64;
+//	ftStatus = FT_SetUSBParameters(ftHandle0, TransferSize, TransferSize);
+//	if (ftStatus == FT_OK) {
+//		printf("In/Out transfer size set to 64 bytes\n");
+//	}
+//	else {
+//		printf("FT_SetUSBParameters failed\n");
+//		retCode = 1;
+//		goto exit;
+//	}
+//
+//
+//exit:
+//	free(Data.Manufacturer);
+//	free(Data.ManufacturerId);
+//	free(Data.Description);
+//	free(Data.SerialNumber);
+//	printf("Returning %d\n", retCode);
+//	return retCode;
+//}
+//
+// unsigned int get_cyclecount (void)
+// {
+//  unsigned int value;
+//	// Read CCNT Register
+//	asm volatile ("MRC p15, 0, %0, c9, c13, 0\t\n": "=r"(value));
+//	return value;
+// }
+//
+// void init_perfcounters (int32_t do_reset, int32_t enable_divider)
+// {
+//	// in general enable all counters (including cycle counter)
+//	int32_t value = 1;
+//
+//	// peform reset:
+//	if (do_reset)
+//	{
+//		value |= 2;     // reset all counters to zero.
+//		value |= 4;     // reset cycle counter to zero.
+//	}
+//
+//	if (enable_divider)
+//		value |= 8;     // enable "by 64" divider for CCNT.
+//
+//	value |= 16;
+//
+//	// program the performance-counter control-register:
+//	asm volatile ("MCR p15, 0, %0, c9, c12, 0\t\n" :: "r"(value));
+//
+//	// enable all counters:
+//	asm volatile ("MCR p15, 0, %0, c9, c12, 1\t\n" :: "r"(0x8000000f));
+//
+//	// clear overflows:
+//	asm volatile ("MCR p15, 0, %0, c9, c12, 3\t\n" :: "r"(0x8000000f));
+// }
+import "C"
+import "fmt"
+import "os"
+import "syscall"
+import "unsafe"
+import "time"
+import "strconv"
+import "strings"
+import "sort"
+import "log"
+
+var timeStampLayout = "02:Jan:2006:15:04:05.000000"
+var samples = 100
+
+func openPort(name string) (f *os.File, err error) {
+
+	f, err = os.OpenFile(name, syscall.O_RDWR|syscall.O_NOCTTY, 0666)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		if err != nil && f != nil {
+			f.Close()
+		}
+	}()
+
+	fd := f.Fd()
+
+	// Set serial port 'name' to 115200/8/N/1 in RAW mode (i.e. no pre-process of received data
+	// and pay special attention to Cc field, this tells the serial port to not return until at
+	// at least syscall.VMIN bytes have been read. This is a tunable parameter they may help in Lab 3
+	t := syscall.Termios{
+		Iflag:  syscall.IGNPAR,
+		Cflag:  syscall.CS8 | syscall.CREAD | syscall.CLOCAL | syscall.B115200,
+		Cc:     [32]uint8{syscall.VMIN: 27},
+		Ispeed: syscall.B115200,
+		Ospeed: syscall.B115200,
+	}
+
+	// Syscall to apply these parameters
+	_, _, errno := syscall.Syscall6(
+		syscall.SYS_IOCTL,
+		uintptr(fd),
+		uintptr(syscall.TCSETS),
+		uintptr(unsafe.Pointer(&t)),
+		0,
+		0,
+		0,
+	)
+
+	if errno != 0 {
+		return nil, errno
+	}
+
+	return f, nil
+}
+
+type int64arr []int64
+func (a int64arr) Len() int { return len(a) }
+func (a int64arr) Swap(i, j int){ a[i], a[j] = a[j], a[i] }
+func (a int64arr) Less(i, j int) bool { return a[i] < a[j] }
+
+func generateTimestamp() string {
+	t := time.Now()
+	return t.Format(timeStampLayout)
+}
+
+func parseTimestamp(timestamp string) time.Time {
+	t, e := time.Parse(timeStampLayout, timestamp)
+	if e != nil {
+		fmt.Printf("Parse error occured: %v\n", e)
+	}
+	return t
+}
+
+
+func calculateDelayRTT(p0 string, p1 string, p2 string, p3 string) int64 {
+	// parse time stamp string
+	t0 := parseTimestamp(p0)
+	t1 := parseTimestamp(p1)
+	t2 := parseTimestamp(p2)
+	t3 := parseTimestamp(p3)
+	delayRTT := (t3.Sub(t0) + t2.Sub(t1))
+	//fmt.Printf("RTT delay: %v\n", delayRTT)
+	return int64(delayRTT.Nanoseconds())
+}
+
+func exchangeTimestamps() int64 {
+	// client = coordinator, server = endpoints. We are server
+	// t0 is the client's timestamp of the request packet transmission,
+	// t1 is the server's timestamp of the request packet reception,
+	t0 := C.GoString(C.readMsg())
+	t1 := generateTimestamp()
+
+	// t2 is the server's timestamp of the response packet transmission and
+	// t3 is the client's timestamp of the response packet reception.
+	t2 := generateTimestamp()
+	C.writeMsg(C.CString(t2))
+	t3 := C.GoString(C.readMsg())
+	// gotta send t1
+	C.writeMsg(C.CString(t1))
+	//calculateClockOffset(t0, t1, t2, t3)
+	return calculateDelayRTT(t0, t1, t2, t3)
+}
+
+func readMessage(f *os.File) string {
+	buffer := make([]byte, 27)
+	_, err := f.Read(buffer)
+	if err != nil {
+		fmt.Printf("Read error occured: %v\n", err)
+	}
+	//fmt.Printf("Received %d bytes: %s\n", count, string(buffer))
+	return string(buffer)
+}
+
+func writeMessage(f *os.File, str string) {
+	buffer := []byte(str)
+	_, err := f.Write(buffer)
+	if err != nil {
+		fmt.Printf("Write error occured: %v\n", err)
+	}
+	//fmt.Printf("Sent %d bytes: %s\n", count, str)
+}
+
+func waitTimer(cycles int64, f *os.File) int {
+	// init counters:
+	C.init_perfcounters(1, 0)
+	//fmt.Printf("cyles to wait: %d\n", cycles)
+	timeStart := C.ulonglong(C.get_cyclecount())
+	timeElapsed := C.ulonglong(0)
+	for {
+		timeElapsed = C.ulonglong(C.get_cyclecount()) - timeStart
+		if timeElapsed > C.ulonglong(cycles) {
+			writeMessage(f, fmt.Sprintf("%27s", "COMPLETE"))
+			break
+		}
+	}
+	return int(timeElapsed)
+}
+
+func priority_test(f *os.File, delay int64) {
+	rtt := delay
+	str := readMessage(f)
+	cycles, err := strconv.ParseInt(strings.TrimSpace(str), 0, 64)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("readMessage string: %s\n", str)
+	fmt.Printf("rtt: %d\n", rtt)
+	fmt.Printf("cycles: %d\n", cycles)
+	waitTimer(cycles-rtt-rtt, f)
+	//writeMessage(f, fmt.Sprintf("%27s", "COMPLETE"))
+}
+
+func calculateDelayJitter() (aDelay int64, aJitter int64) {
+	samples := 100
+	delayEntries := make([]int64, samples)
+	// Delay/Jitter Calculations
+	//totalDelay := uint64(0)
+	for i := 0; i < samples; i++ {
+		delayEntries[i] = exchangeTimestamps()
+		log.Printf("%v\n", i)
+		//totalDelay += delayEntries[i]
+	}
+	//avgdelay := totalDelay / uint64(samples)
+	sort.Sort(int64arr(delayEntries))
+	avgdelay := (delayEntries[49] + delayEntries[59])/2
+	totaljitter := int64(0)
+	jitter := int64(0)
+	extremes := 0
+	jitterEntries := make([]int64, samples)
+	for i := 0; i < samples; i++ {
+		if delayEntries[i] > 300000000 {
+			extremes += 1
+			continue
+		}
+		if delayEntries[i] > avgdelay {
+			jitter = delayEntries[i] - avgdelay
+		} else {
+			jitter = avgdelay - delayEntries[i]
+		}
+		jitterEntries[i] = jitter;
+		//fmt.Printf("Cur Jitter = %d \n", jitter)
+		totaljitter += jitter
+	}
+	sort.Sort(int64arr(jitterEntries))
+	samples -= extremes
+	avgjitter := totaljitter / int64(samples)
+	fmt.Printf("----------------------Delay Entries----------------------------------\n");
+	for i := 0; i < samples; i++ {
+		fmt.Printf("%v\n", delayEntries[i]);
+	}
+	fmt.Printf("----------------------Jitter Entries---------------------------------\n");
+	for i := 0; i < samples; i++ {
+		fmt.Printf("%v\n", jitterEntries[i]);
+	}
+	fmt.Printf("----------------------Final Statistics--------------------------------\n");
+	fmt.Printf("median  delay  = %v \n", avgdelay);
+	fmt.Printf("average jitter = %v \n", avgjitter);
+	fmt.Printf("Num outliers = %v \n", extremes)
+	return int64(avgdelay), int64(avgjitter)
+}
+
+func main() {
+	C.setup()
+	//avgDelay, avgJitter := calculateDelayJitter()
+	calculateDelayJitter()
+	//fmt.Printf("Avg. Delay = %v ns\n", avgDelay)
+	//fmt.Printf("Avg. Jitter = %v ns\n", avgJitter)
+	//priority_test(f, avgDelay)
+}
